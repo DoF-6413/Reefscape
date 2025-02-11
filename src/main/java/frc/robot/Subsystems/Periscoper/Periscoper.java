83472b64df05411718022f018f9b48a85e882a93
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2025-02-10 18:20:22 -0700

    create the periscoper.java file and finish it

--- src/main/java/frc/robot/Subsystems/Periscoper/Periscoper.java
+++ src/main/java/frc/robot/Subsystems/Periscoper/Periscoper.java
@@ -4,14 +4,41 @@
 
 package frc.robot.Subsystems.Periscoper;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Periscoper extends SubsystemBase {
+
+  private final Periscoper m_Periscoper;
+  private final PeriscoperIO io;
+  private final PeriscoperIOInputsAutoLogged inputs = new PeriscoperIOInputsAutoLogged();
+
   /** Creates a new Periscoper. */
-  public Periscoper() {}
+  public Periscoper(PeriscoperIO periscoperIO) {
+    //initialize the Periscoper subsystem
+    System.out.println("[Init] Creating Drive");
+    this.io = periscoperIO;
+
+    m_Periscoper = new Periscoper(periscoperIO);
+
+  }
 
   @Override
+  // This method will be called once per scheduler run
   public void periodic() {
-    // This method will be called once per scheduler run
+    m_Periscoper.periodic();
+    Logger.processInputs("Periscoper", inputs);
   }
+
+  public void updateInputs(){
+    io.updateInputs(inputs);
+  }
+
+  public void setVoltage(double volts){
+    io.setVoltage(volts);
+  }
+  public void setVelocity(double velocity){
+    io.setVelocity(velocity);
+}
 }
