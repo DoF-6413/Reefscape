56ef7cea9de6f83e5684428d0c7c29f64e5cdf24
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2025-02-11 17:27:34 -0700

    adding the max and min agle to the constants

--- src/main/java/frc/robot/Subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.Subsystems.Climber;
 
+import edu.wpi.first.math.util.Units;
+
 public final class ClimberConstants {
     public static final int CAN_ID = 17; //CAN ID for climb motor
     //fake constants TODO: find real values and add more
@@ -13,5 +15,7 @@ public final class ClimberConstants {
     public static final double UPDATE_FREQUENCY_HZ = 100;
     public static final int CUR_LIM_A = 50;
     public static final boolean ENABLE_CUR_LIM = true;
+    public static final double CLIMBER_MIN_ANGLE_RAD = 0;
+    public static final double CLIMBER_MAX_ANGLE_RAD = Units.degreesToRadians(80);
     
 }
