a5b0381043c0cf4fc2301e951e2ba67b3a13ace4
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2025-02-11 17:24:19 -0700

    finish the climber.java file

--- src/main/java/frc/robot/Subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/Subsystems/Climber/Climber.java
@@ -1,5 +1,40 @@
 package frc.robot.Subsystems.Climber;
 
-public class Climber {
-    
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+
+  private final Climber m_Climber;
+  private final ClimberIO io;
+  private final ClimberIOInputsAutoLogged inputs = new ClimberIOInputsAutoLogged();
+
+  /** Creates a new Climber. */
+  public Climber(ClimberIO ClimberIO) {
+    //initialize the Climber subsystem
+    System.out.println("[Init] Creating Drive");
+    this.io = ClimberIO;
+
+    m_Climber = new Climber(ClimberIO);
+
+  }
+
+  @Override
+  // This method will be called once per scheduler run
+  public void periodic() {
+    m_Climber.periodic();
+    Logger.processInputs("Climber", inputs);
+  }
+
+  public void updateInputs(){
+    io.updateInputs(inputs);
+  }
+
+  public void setClimberVoltage(double volts){
+    io.setClimberVoltage(volts);
+  }
+  public void setClimberVelocity(double velocity){
+    io.setClimberVelocity(velocity);
+}
 }
