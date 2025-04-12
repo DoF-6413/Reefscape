782323af6e74573dd9fbb2644d67263fb3b68c57
Andres Lara <Andres.Lara@sisepuedefoundation.org>
2025-04-12 12:47:30 -0700

    comments to know twhat they do , needs clean

--- src/main/java/frc/robot/Utils/Mechanisms2d.java
+++ src/main/java/frc/robot/Utils/Mechanisms2d.java
@@ -8,23 +8,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.Periscope.Periscope;
 
 public class Mechanisms2d extends SubsystemBase {
-  private final Mechanism2d m_SwerveBase;
-  private final MechanismRoot2d m_periscopeRoot;
-  private final MechanismLigament2d m_periscopeLigament;
-  private final Periscope m_periscope;
+  private final Mechanism2d m_SwerveBase; // area that the robot uses in total + some extra space to look good 
+  private final MechanismRoot2d m_periscopeRoot; // position of the periscope in the robot area 
+  private final MechanismLigament2d m_periscopeLigament; // the periscope itself( measuremnets and movement)
+  private final Periscope m_periscope; // periscope subsystem
 
   public Mechanisms2d(Periscope m_periscope) {
-    m_SwerveBase = new Mechanism2d(.9144, 2.23012);
+    m_SwerveBase = new Mechanism2d(.9144, 2.23012); // 3ft by 7.3ft total area
 
     this.m_periscope = m_periscope;
 
-    m_periscopeRoot = m_SwerveBase.getRoot("Periscope", 0.0762, 0.04445);
-    m_periscopeLigament = m_periscopeRoot.append(new MechanismLigament2d("Periscope", 0.9652, 90));
+    m_periscopeRoot = m_SwerveBase.getRoot("Periscope", 0.0762, 0.04445); // position of the periscope in the robot area
+    m_periscopeLigament = m_periscopeRoot.append(new MechanismLigament2d("Periscope", 0.9652, 90)); // 90 degrees is going upwards
   }
 
   @Override
   public void periodic() {
     m_periscopeLigament.setLength(m_periscope.getHeightMeters());
-    SmartDashboard.putData("Mechanisms2d", m_SwerveBase);
+    SmartDashboard.putData("Mechanisms2d", m_SwerveBase); // logging and visualizing the mechanism 
   }
 }
